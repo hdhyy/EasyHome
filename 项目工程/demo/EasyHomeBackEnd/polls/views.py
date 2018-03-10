d502e45f8e4daddf8e6c21554ff5256de09642d4
hdhyy <fuyunhuayu@foxmail.com>
2018-03-10 22:58:16 +0800

    add the back end project use django
    1.use the django framework
    2.generate the table which used by django
    3.add the mysql support

---
+++ 项目工程/demo/EasyHomeBackEnd/polls/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello,this is poll app's index")
+
+# Create your views here.
