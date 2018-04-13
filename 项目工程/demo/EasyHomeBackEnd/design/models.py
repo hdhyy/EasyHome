7f031b214f456d8ac8e9a4528f4090e73f1c58d3
hdhyy <fuyunhuayu@foxmail.com>
2018-04-13 16:04:24 +0800

    list work function implement

--- 项目工程/demo/EasyHomeBackEnd/design/models.py
+++ 项目工程/demo/EasyHomeBackEnd/design/models.py
@@ -27,8 +27,8 @@ class Model(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     upload_time = models.DateTimeField(auto_now_add=True)
     file = models.FileField(upload_to=upload_to)
-    modelType = models.ForeignKey(ModelType, on_delete=models.CASCADE, default=None)
-    furnitureType = models.ForeignKey(FurnitureType, on_delete=models.CASCADE, default=None)
+    modelType = models.ForeignKey(ModelType, on_delete=models.SET_DEFAULT, default=1)
+    furnitureType = models.ForeignKey(FurnitureType, on_delete=models.SET_DEFAULT, default=1)
 
     def getdicname(self):
         return 'model'
@@ -44,11 +44,17 @@ class Texture(models.Model):
         return 'texture'
 
 
+class SceneType(models.Model):
+    name = models.CharField(max_length=20)
+    extension = models.CharField(max_length=10)
+
+
 class Work(models.Model):
     name = models.CharField(max_length=20)
     generate_time = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     browseNum = models.IntegerField(default=0)
+    sceneType = models.ForeignKey(SceneType, on_delete=models.SET_DEFAULT, default=1)
     file = models.FileField(upload_to=upload_to)
 
     def getdicname(self):
--- 项目工程/demo/EasyHomeBackEnd/design/static/design/myjs/jsonloaderapp.js
+++ 项目工程/demo/EasyHomeBackEnd/design/static/design/myjs/jsonloaderapp.js
@@ -46,42 +46,9 @@ var objloaderapp = (function () {
         this.controls = new THREE.TrackballControls(this.camera, this.renderer.domElement);
     };
 
-    objloaderapp.prototype.initLight = function () {
-        var ambientLight = new THREE.AmbientLight(0x404040);
-        var directionalLight1 = new THREE.DirectionalLight(0xC0C090);
-        var directionalLight2 = new THREE.DirectionalLight(0xC0C090);
-
-        directionalLight1.position.set(-100, -50, 100);
-        directionalLight2.position.set(100, 50, -100);
-
-        var hemispherelight = new THREE.HemisphereLight(0xffffff, 0x444444);
-        hemispherelight.position.set(0, 200, 0);
-
-        //this.scene.add(hemispherelight);
-        //this.scene.add(directionalLight1);
-        //this.scene.add(directionalLight2);
-        //this.scene.add(ambientLight);
-    };
-
     objloaderapp.prototype.initHelper = function () {
         var helper = new THREE.GridHelper(1200, 60, 0xFF4444, 0x404040);
         this.scene.add(helper);
-
-        var grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
-        grid.material.opacity = 0.2;
-        grid.material.transparent = true;
-        //this.scene.add(grid);
-    };
-
-    objloaderapp.prototype.initObject = function () {
-        // ground
-        var mesh = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({
-            color: 0x5f5f5f,
-            depthWrite: false
-        }));
-        mesh.rotation.x = -Math.PI / 2;
-        mesh.receiveShadow = true;
-        //this.scene.add(mesh);
     };
 
     objloaderapp.prototype.initContent = function () {
@@ -89,11 +56,7 @@ var objloaderapp = (function () {
         var objectLoader = new THREE.ObjectLoader();
         var myscene = this.scene;
         objectLoader.load(this.url, function (obj) {
-            if (obj instanceof THREE.Scene) {
-                myscene.add(obj);
-            } else {
-                myscene.add(obj);
-            }
+            myscene.add(obj);
         });
     };
 
